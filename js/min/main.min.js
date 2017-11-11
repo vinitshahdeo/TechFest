6fb069f0636345542763dd76225aad1c44825aab
Vinit Shahdeo <vinitshahdeo@gmail.com>
2017-11-12 02:55:14 +0530

    javascript library used

---
+++ js/min/main.min.js
@@ -0,0 +1,1 @@
+!function(e){"use strict";e(".navigation").singlePageNav({currentClass:"active"}),e(".toggle-menu").click(function(){return e(".responsive-menu").stop(!0,!0).slideToggle(),!1})}(jQuery);
